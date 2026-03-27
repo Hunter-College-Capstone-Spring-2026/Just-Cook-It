dfc83b69371c02eb572dc003f5a0beb679eb93a5
kirillk04 <kirillkheyfets04@gmail.com>
2026-03-27 19:58:04 -0400

    deitary restrictions save on db

--- backend/app/utils/config.py
+++ backend/app/utils/config.py
@@ -19,6 +19,8 @@ class Settings:
     supabase_service_role_key: str = os.getenv("SUPABASE_SERVICE_ROLE_KEY", "")
     supabase_user_table: str = os.getenv("SUPABASE_USER_TABLE", "User")
     supabase_state_table: str = os.getenv("SUPABASE_STATE_TABLE", "UserAppState")
+    supabase_dietary_restriction_table: str = os.getenv("SUPABASE_DIETARY_RESTRICTION_TABLE", "DietaryRestriction")
+    supabase_user_dietary_table: str = os.getenv("SUPABASE_USER_DIETARY_TABLE", "UserDietaryRestriction")
     supabase_ingredient_table: str = os.getenv("SUPABASE_INGREDIENT_TABLE", "Ingredient")
     supabase_user_ingredient_table: str = os.getenv("SUPABASE_USER_INGREDIENT_TABLE", "UserIngredient")
 
